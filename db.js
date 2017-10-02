f642e8eb6d4f1c207af9307e6f5c8e11d2e5cb9a
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2017-10-01 20:57:16 -0400

    sketched the models

--- db.js
+++ db.js
@@ -6,15 +6,51 @@ const db = new Sequelize('postgres://localhost/juniorenrichment', {
 });
 
 
-const Student = db.define("student" , {
+const Student = db.define('student', {
 	/* STUDENT MODEL CODE HERE */
+	name: {
+		type: Sequelize.STRING
+	},
+	GPA: {
+		type: Sequelize.INTEGER
+	},
+	teacherId: {
+		type: Sequelize.INTEGER
+	}
 
-})
-	
+});
+
+Student.prototype.getLetterGrade = function() {
+	if (this.GPA === 4) {
+		return 'A';
+	} else if (this.GPA === 3) {
+		return 'B';
+	} else if (this.GPA === 2) {
+		return 'C';
+	} else if (this.GPA === 1) {
+		return 'D';
+	} else {
+		return 'F';
+	}
+};
+
+Student.getAllAStrudents = function() {
+	Student.findAll({
+		where: {
+			GPA: 4
+		}
+	});
+};
+
 
 const Teacher = db.define('teacher', {
 	/* TEACHER MODEL CODE HERE */
-
+	name: {
+		type: Sequelize.STRING
+	},
+	subject: {
+		type: Sequelize.STRING
+	}
 });
 
 
